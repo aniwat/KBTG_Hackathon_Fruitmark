0df6a137ab18432a699d342a6ff3571b6056d33c
Aniwat Keereena <aniwat_keereena@hotmail.com>
2018-12-02 03:14:21 +0700

    Add test web

--- src/main/java/com/kbtg/hackathon/fruitmark/controller/DatabaseController.java
+++ src/main/java/com/kbtg/hackathon/fruitmark/controller/DatabaseController.java
@@ -5,28 +5,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kbtg.hackathon.fruitmark.dao.MerchantRepository;
-import com.kbtg.hackathon.fruitmark.entity.Merchant;
+import com.kbtg.hackathon.fruitmark.dao.ProductRepository;
+import com.kbtg.hackathon.fruitmark.entity.Product;
 
 @RestController
 public class DatabaseController {
 	
 	@Autowired
-	MerchantRepository repo;
+	MerchantRepository merchantRepo;
+	
+	@Autowired
+	ProductRepository productRepo;
 	
 	@RequestMapping("/database")
 	public String index() {
 		System.out.println("Call DatabaseController");
 		
-		Iterable<Merchant> list = repo.findAll();
+		Iterable<Product> list = productRepo.findPrdByKeyword("%" + "น้ำหอม" + "%");
 		if (list != null) {
 			System.out.print("list != null");
-			for (Merchant m : list) {
-				System.out.println("m.getMerchantName() = " + m.getMerchantName());
-				System.out.println("m.getMerchantId() = " + m.getMerchantId());
+			for (Product p : list) {
+				System.out.println("p.getProductName() = " + p.getProductName());
+				System.out.println("p.getPricePerUnit() = " + p.getPricePerUnit());
+				System.out.println("p.getProductImg() = " + p.getProductImg());
 			}
 		} else {
 			System.out.print("list == null");
 		}
+		
+		// Iterable<Merchant> list = merchantRepo.findAll();
+		// if (list != null) {
+		// System.out.print("list != null");
+		// for (Merchant m : list) {
+		// System.out.println("m.getMerchantName() = " + m.getMerchantName());
+		// System.out.println("m.getMerchantId() = " + m.getMerchantId());
+		// }
+		// } else {
+		// System.out.print("list == null");
+		// }
 		
 		return "Database!";
 	}
