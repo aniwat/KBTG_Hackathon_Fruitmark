9d93a59ce69ae2493070da392a7bb812583c8071
ckenkub <ckenkub@gmail.com>
2018-12-01 23:28:21 +0700

    handleText

--- src/main/java/com/kbtg/hackathon/fruitmark/controller/LineBotController.java
+++ src/main/java/com/kbtg/hackathon/fruitmark/controller/LineBotController.java
@@ -2,10 +2,7 @@ package com.kbtg.hackathon.fruitmark.controller;
 
 import static java.util.Arrays.asList;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -30,7 +27,6 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.ImageMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
@@ -62,6 +58,61 @@ public class LineBotController {
 		
 		String text = event.getMessage().getText();
 		System.out.println("text: " + text);
+		handleText(text,replyToken);
+	}
+	
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("Class LineBotController");
+		System.out.println("Method handleDefaultMessageEvent");
+		System.out.println("Event: " + event);
+	}
+	
+	@EventMapping
+	public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+		handleSticker(event.getReplyToken(), event.getMessage());
+	}
+	
+	private void handleSticker(String replyToken, StickerMessageContent content) {
+		reply(replyToken, new StickerMessage(content.getPackageId(), content.getStickerId()));
+	}
+	
+	private void reply(@NonNull String replyToken, @NonNull Message message) {
+		reply(replyToken, Collections.singletonList(message));
+	}
+	
+	private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+		try {
+			BotApiResponse apiResponse = lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
+			System.out.printf("Sent messages: {}", apiResponse);
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@EventMapping
+	public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
+		System.out.println(event.toString());
+		ImageMessageContent content = event.getMessage();
+		String replyToken = event.getReplyToken();
+		
+		try {
+			MessageContentResponse response = lineMessagingClient.getMessageContent(content.getId()).get();
+			DownloadedContent downloadedContent = new DownloadedContent();
+			DownloadedContent jpg = downloadedContent.saveContent("jpg", response);
+			
+			ImageAnalyze image = new ImageAnalyze();
+			String keyW0rd = image.analzye(jpg.getUri());
+			
+			handleText(keyW0rd,replyToken);
+			
+		} catch (InterruptedException | ExecutionException e) {
+			handleText("ไม่รู้จัก",replyToken);
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private  void handleText(String text, String replyToken) {
 		
 		String response = "ไม่รู้จักคำสั่ง";
 		if (text.startsWith("หาร้านค้า")) {
@@ -111,77 +162,7 @@ public class LineBotController {
 		}
 		
 		this.reply(replyToken, new TextMessage(response));
-	}
-	
-	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
-		System.out.println("Class LineBotController");
-		System.out.println("Method handleDefaultMessageEvent");
-		System.out.println("Event: " + event);
-	}
-	
-	@EventMapping
-	public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
-		handleSticker(event.getReplyToken(), event.getMessage());
-	}
-	
-	private void handleSticker(String replyToken, StickerMessageContent content) {
-		reply(replyToken, new StickerMessage(content.getPackageId(), content.getStickerId()));
-	}
-	
-	private void reply(@NonNull String replyToken, @NonNull Message message) {
-		reply(replyToken, Collections.singletonList(message));
-	}
-	
-	private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
-		try {
-			BotApiResponse apiResponse = lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
-			System.out.printf("Sent messages: {}", apiResponse);
-		} catch (InterruptedException | ExecutionException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@EventMapping
-	public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
-		System.out.println(event.toString());
-		ImageMessageContent content = event.getMessage();
-		String replyToken = event.getReplyToken();
 		
-		try {
-			MessageContentResponse response = lineMessagingClient.getMessageContent(content.getId()).get();
-			
-			DownloadedContent downloadedContent = new DownloadedContent();
-			
-			DownloadedContent jpg = downloadedContent.saveContent("jpg", response);
-			// DownloadedContent previewImage = downloadedContent.createTempFile("jpg");
-			
-			// system("convert", "-resize", "240x", jpg.getPath().toString(), previewImage.getPath().toString());
-			System.out.println("original " + jpg.getUri());
-			ImageAnalyze image = new ImageAnalyze();
-			String keyW0rd = image.analzye(jpg.getUri());
-			System.out.println("WORD :  " + keyW0rd);
-			this.reply(event.getReplyToken(), new TextMessage(keyW0rd));
-			// reply(replyToken, new ImageMessage(jpg.getUri(), previewImage.getUri()));
-			
-		} catch (InterruptedException | ExecutionException e) {
-			// reply(replyToken, new TextMessage("Cannot get image: " + content));
-			throw new RuntimeException(e);
-		}
-	}
-	
-	private void system(String... args) {
-		ProcessBuilder processBuilder = new ProcessBuilder(args);
-		try {
-			Process start = processBuilder.start();
-			int i = start.waitFor();
-			System.out.println(String.format("result: %s => %s", Arrays.toString(args), i));
-		} catch (InterruptedException e) {
-			System.out.println("Interrupted " + e);
-			Thread.currentThread().interrupt();
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
-		}
 	}
 	
 }
